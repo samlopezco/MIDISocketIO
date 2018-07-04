a3832a0401fe56a19c9f35ce3a564b6f0d50170f
Sam Lopez <sam.lopez010@gmail.com>
2018-07-04 18:41:09 -0500

    Broadcasting / Receiving Sockets
    SocketIO added

--- Public/index.js
+++ Public/index.js
@@ -2,6 +2,19 @@
 var context = new AudioContext();
 var oscillators = {};
 var midi, data;
+var socket = io();
+socket.on('externalMidi', gotExternalMidiMessage);
+
+function gotExternalMidiMessage(data){
+    var newItem = document.createElement('li');
+    newItem.appendChild(document.createTextNode('Note: ' + data.pitch + ' Velocity:' + data.velocity));
+    newItem.className = "external-midi";
+    document.getElementById('midi-data').prepend(newItem);
+
+    playNote(data);
+}
+
+
 if (navigator.requestMIDIAccess) {
   navigator.requestMIDIAccess({
     sysex: false
@@ -29,8 +42,44 @@ function onMIDImessage(messageData) {
     newItem.appendChild(document.createTextNode(messageData.data));
     newItem.className = 'user-midi';
     document.getElementById('midi-data').prepend(newItem);
+    socket.emit('midi', note);
   }
   //Values ON / OFF -> 144 / 128 
   //NOTE -> 0-127, C2 = 48
   //Velocity -> 0-127
-  
+  /////////////////////////////////////
+  ///       MIDI PLAYBACK / AUDIO   ///
+  /////////////////////////////////////
+var d = messageData.data; // Example: [144, 60, 100]
+var note = {
+  on: d[0], 
+  pitch: d[1],
+  velocity: d[2]
+};
+play(note);
+
+
+//Checks whether note is on/off reference to global variables
+function play(note) {
+    switch(note.on){
+        case 144:
+            noteOn (frequency(note.pitch), note.velocity);
+            break;
+        case 128:
+            noteOff (frequency(note.pitch), note.velocity);
+            break;
+    }
+    //MIDI NOTE TO PITCH
+    // f(n) = 2^((n-49)/12) * 440HZ
+    function frequency(note){
+        return Math.pow(2, ((note - 69) / 12))*440;
+      }
+        var osc = oscillators[frequency] = context.createOscillator();
+        osc.type = 'sawtooth';
+        osc.connect(context.destination);
+        osc.start(context.currentTime);
+    }  
+    function noteOff(frequency, velocity){
+        oscillators[frequency].stop(context.currentTime);
+        oscillators[frequency].disconnect();
+      }
--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 /*jshint esversion: 6 */
 var express = require('express');
 var app = express();
+var socket = require('socket.io');
+var io = require('socket.io').listen(server);
 const port = process.env.port || 8080;
 var server = app.listen(port);
 
 app.use(express.static('public'));
+
+
+io.sockets.on('connection', newConnection);
+function newConnection(socket){
+    console.log('Added Connection from: ' + socket.id);
+    socket.on('midi', midiMsg);
+    function midiMsg(data){
+        socket.broadcast.emit('externalMidi', data);
+    }
+}
+
