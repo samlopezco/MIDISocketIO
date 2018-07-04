eb6494364f6b7a69ff56abe71b426b95fae83d15
Sam Lopez <sam.lopez010@gmail.com>
2018-07-04 18:53:55 -0500

    HTML + CSS added

--- Public/index.js
+++ Public/index.js
@@ -83,3 +83,22 @@ function play(note) {
         oscillators[frequency].stop(context.currentTime);
         oscillators[frequency].disconnect();
       }
+
+///////////////////
+// CONTROLLER   //
+/////////////////
+var allPads = document.getElementsByClassName('pad'),
+    dataList = document.querySelector('#midi-data ul');
+
+for (var i=0; i<allPads.length; i++) {
+  allPads[i].addEventListener('mousedown', function() {
+    var newItem = document.createElement('li');
+    newItem.appendChild(document.createTextNode('[144,'+this.dataset.midi+',64]'));
+    dataList.appendChild(newItem);
+  });
+    allPads[i].addEventListener('mouseup', function() {
+    var newItem = document.createElement('li');
+    newItem.appendChild(document.createTextNode('[128,'+this.dataset.midi+',127]'));
+    dataList.appendChild(newItem);
+  });
+}
